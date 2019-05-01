52fbfc200c305eb7365cb14e4dc341bf2bb9dfdd
Tuikku Anttila <tuikku.anttila@gmail.com>
2019-05-01 22:35:41 +0300

    Batching code handles both string arrays and dict arrays suitable for boto3

--- kinesis_stream_inserter.py
+++ kinesis_stream_inserter.py
@@ -1,20 +1,55 @@
 import boto3
+import random
+import time
+
+from kinesisdataformatter import KinesisBatcher
+
 
 client = boto3.client('kinesis')
 
 
-if __name__ == "__main__":
+def get_random_sample_records():
 
 	records = []
-	for i in range(10):
-		data = "this is record number " + str(i)
-		record = {'Data' : data, 'PartitionKey' : 'testRecords'}
+
+	for i in range(random.randint(1, 5)):
+
+		partition_key = "partition-{}".format(i)
+		record = {'Data' : b'a string', 'PartitionKey' : partition_key}
 		records.append(record)
 
-	print("Putting records to stream...")
-	response = client.put_records(
-	    Records=records,
-	    StreamName='Tuikku-Dev-Stream'
-	)
-	print(response)
+	return records
+
+if __name__ == "__main__":
+
+	# Initialise batching code
+	batcher = KinesisBatcher(record_max_size=100, batch_max_size=500, max_records_per_batch=2, formatting="json")
+
+	while True:
+
+		records = get_random_sample_records()
+		print(records)
+		print("****")
+		for batch in batcher.batch_data(records):
+
+
+			print("Putting {} records to stream...".format(len(batch)))
+			response = client.put_records(
+			    Records=batch,
+			    StreamName='Tuikku-Dev-Stream'
+			)
+			print(response)
+
+		time.sleep(5)
+
+			# Here you'd check for errors
+			# and possibly re-send 
+
+
+
+	# records = []
+	# for i in range(10):
+	# 	data = "this is record number " + str(i)
+	# 	record = {'Data' : data, 'PartitionKey' : 'testRecords'}
+	# 	records.append(record)
 
--- kinesisdataformatter.py
+++ kinesisdataformatter.py
@@ -20,71 +20,110 @@
 # If they are sent to AWS there's an issue that sending of a record in the
 # batch could fail and then the result would no longer be in the same order..
 
+ALLOWED_FORMATTING_VALUES = ["string", "json"]
 
 
 class KinesisBatcher:
+
+
 	# TODO should we keep track of almost filled records for optimizing?
 	# No because then they would not stay in the same order
 
-	def __init__(self, data, record_max_size=10, batch_max_size=50, max_records_per_batch=5):
-		self.unmodified_data = data
+	def __init__(self, record_max_size=10, batch_max_size=50, max_records_per_batch=5, formatting="string"):
+		self.unmodified_data = []
 		self.record_max_size = record_max_size
 		self.batch_max_size = batch_max_size
 		self.max_records_per_batch = max_records_per_batch
 		self.discarded = 0
 
+		if formatting not in ALLOWED_FORMATTING_VALUES:
+			raise ValueError("{} is not allowed as a format, allowed values are {}".format(formatting, ALLOWED_FORMATTING_VALUES))
+
+		self.format = formatting
+
 	def is_too_large(self, record):
 
-		return len(record) > self.record_max_size
+		return self.get_record_size(record) > self.record_max_size
 
-	def get_data(self):
-		for d in self.unmodified_data:
+	def get_data(self, data):
+
+		for d in data:
+			print("Now getting next item")
+			print(d)
+
+			#if not isinstance(d, str):
+			#	self.nonstring += 1
+			#	continue
+
 			if self.is_too_large(d):
 				# Simply discard items that are too large
 				self.discarded += 1
 				continue
+
 			yield d
+
 		return
 
-	def get_batch_size(self, batch):
-		# TODO return sum of records in bytes
-		# TODO keep track of sum somewhere else instead
-		# of returning it here
-		return len(batch)
-
 	def get_record_size(self, record):
-		# TODO return size in bytes 
-		return len(record)
+		
 
-	def batch_data(self):
+		if self.format == "json":
+			try:
+				return len(record["Data"]) + len(record["PartitionKey"].encode("utf-8"))
+			except KeyError:
+				raise ValueError("Record {} was invalid json for Kinesis".format(record))
+			except AttributeError:
+				raise ValueError("Record {} invalid; Expected partition key to be string, data bytes".format(record))
 
+		elif self.format == "string":
+			return len(record.encode("utf-8"))
+
+	def batch_data(self, data):
+		print("Starting batch operations...")
 		batch = []
-		data_for_batch = self.get_data()
+		print(data)
+
+		data_for_batch = self.get_data(data)
+		current_batch_size = 0
+		current_records_in_batch = 0
 
 		while True:
 
 			try:
 
+				print("Getting item from iterator...")
 				next_item = next(data_for_batch)
-
-				if self.get_batch_size(batch) + self.get_record_size(next_item) > self.batch_max_size or len(batch) > self.max_records_per_batch:
+				print(next_item)
+				if current_batch_size + self.get_record_size(next_item) > self.batch_max_size or current_records_in_batch == self.max_records_per_batch:
+					# This batch couldn't fit any more items, so return it
 					yield batch
+
+					# Reset counters
 					batch = []
+					current_batch_size = 0
+					current_records_in_batch = 0
+
 				batch.append(next_item)
+				current_batch_size += self.get_record_size(next_item)
+				current_records_in_batch += 1
 
 			except StopIteration:
 
 				print("All data batched")
-				print("Discarded {} items that went over the max record size limit of {}".format(self.discarded, self.batch_max_size))
-
+				print("Discarded {} records that went over the max record size limit of {}".format(self.discarded, self.batch_max_size))
+				yield batch
 				return
 
 
 if __name__ == "__main__":
 
-	batcher = KinesisBatcher(["i", "ii", "iii", "i", "i", "iw", "iasda", "a"])
+	batcher = KinesisBatcher(["i", 2, "ii", "iii", "i", "i", "iw", "iasda", "a"])
 	iterator = batcher.batch_data()
 	print(next(iterator))
+	print(next(iterator))
 
-
+	## Instead of initialising with data
+	# should we batcher = KinesisBatcher(maxdata=1)
+	# batcher.batch(["a", "b", "c"])
+	# for data in batcher.batch(["a", "b", "c"])
 
